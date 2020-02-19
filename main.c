12912085e29fd7118f6ead1b37ccd21815611042
Bradley Laurie <blaurie@users.noreply.github.com>
2020-02-19 09:22:21 -0600

    Okay

---
+++ main.c
@@ -0,0 +1,17 @@
+
+/**
+ * Add a submodule with:
+ * 		git submodule add <git repository> extern/<repo name>
+ *
+ * 	adding in to the extern folder or something like it makes management of external deps a bit easier.
+ *
+ * Updating submodules from master commits:
+ * 		git submodule foreach git pull origin master
+ */
+
+#include <collection/array.h>
+
+
+int main(void) {
+	return 0;
+}
